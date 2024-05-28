aaafd8fecf3771d1533e4dca006ca3e5ea9f9ea7
Bankin <bankin@softuni>
2024-05-28 19:24:03 +0300

    Add some comments to HomeController

--- src/main/java/bg/softuni/pathfinder/web/HomeController.java
+++ src/main/java/bg/softuni/pathfinder/web/HomeController.java
@@ -3,10 +3,12 @@ package bg.softuni.pathfinder.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Random;
 
+/**
+ * Controller handling the info pages - home, about, contact
+ */
 @Controller
 public class HomeController {
 
