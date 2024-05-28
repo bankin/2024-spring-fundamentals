41942b38312a54426578c93df6612266e2e80944
Bankin <bankin@softuni>
2024-05-28 19:23:06 +0300

    Add some comments to RouteController

--- src/main/java/bg/softuni/pathfinder/web/RouteController.java
+++ src/main/java/bg/softuni/pathfinder/web/RouteController.java
@@ -2,24 +2,30 @@ package bg.softuni.pathfinder.web;
 
 import bg.softuni.pathfinder.service.RouteService;
 import bg.softuni.pathfinder.service.dto.RouteShortInfoDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+/**
+ * Controller to handle all things route relates
+ */
 @Controller
 public class RouteController {
-    private RouteService routeService;
+    private final RouteService routeService;
 
     public RouteController(RouteService routeService) {
         this.routeService = routeService;
     }
 
+    /**
+     * Method to handle the listing of all routes.
+     *
+     * @return the list routes view
+     */
     @GetMapping("/routes")
     public String routes(Model model) {
-//        RouteShortInfoDTO randomRoute = routeService.getRandomRoute();
         List<RouteShortInfoDTO> routes = routeService.getAll();
 
         model.addAttribute("allRoutes", routes);
